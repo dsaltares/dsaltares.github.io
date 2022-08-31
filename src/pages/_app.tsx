5c9437ff9ad2ab9f91334a804af77b7472f37a53
David Saltares <david.saltares@gmail.com>
2022-08-31 12:50:44 +0300

    feat: favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <title>My new cool app</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
 
-export default MyApp;
+export default App;
