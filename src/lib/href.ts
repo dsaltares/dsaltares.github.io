612e42d3c10a26b8679991c8275ad80533854fc5
David Saltares <david.saltares@gmail.com>
2022-09-01 10:07:38 +0300

    fix: url issue

--- src/lib/href.ts
+++ src/lib/href.ts
@@ -2,6 +2,7 @@ export const titleToHref = (title: string) =>
   title
     .toLowerCase()
     .replace(/ /g, '-')
+    .replace(/[+-]/g, '-')
     .replace(/[^a-z0-9-]/g, '');
 
 export const categoryToHref = (category: string) =>
