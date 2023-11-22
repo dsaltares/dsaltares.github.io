39011d745cc56e65e50a966bdd71969568f40724
David Saltares <david.saltares@gmail.com>
2023-11-22 15:05:33 +0200

    fix: remove deprecated export command

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    images: {
-      unoptimized: true,
-    },
+  images: {
+    unoptimized: true,
   },
   trailingSlash: true,
 };
